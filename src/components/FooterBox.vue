567f4abd0a1060cd6196a0f84249e855a7275ed1
LorenzoBuongiorno <lorenzo.buongiorno00@gmail.com>
2022-02-09 17:47:15 +0100

    about the blog

--- src/components/FooterBox.vue
+++ src/components/FooterBox.vue
@@ -6,14 +6,28 @@
                 <span class="get-in-touch">Get in Touch</span>
             </div>
         </div>
-            <div class="footer-content"></div>
+            <div class="footer-content flex-just-center">
+                <div class="row">
+                    <div class="footer-box">
+                        <h2>ABOUT THE BLOG</h2>
+                        <p class="about-blog">{{blogInfo}}</p>
+                    </div>
+                    <div class="footer-box"></div>
+                    <div class="footer-box"></div>
+                </div>
+            </div>
             <div class="footer-bottom-bar"></div>
     </div>
 </template>
 
 <script>
 export default {
-    
+    props: {
+        blogInfo: String,
+        recentPosts: Array,
+        recentComments: Array,
+         categories: Array
+    }
 }
 </script>
 
@@ -43,7 +57,23 @@ export default {
         }
     }
     .footer-content{
+        padding-top: 30px;
         background-color: #212529;
+        .footer-box{
+            width: calc(100% / 3 - 30px);
+            margin: 0px 15px;
+        }
+        .about-blog{
+            height: 150px;
+            line-height: 30px;
+            overflow: hidden;
+            text-overflow: ellipsis;
+            color: #888;
+        }
+        h2{
+               margin-bottom: 20px;
+               color: $light-color;
+           }
     }
 
 </style>
