ba87084d57d9e8f6813fc5a5cba16a5964af10d8
LorenzoBuongiorno <lorenzo.buongiorno00@gmail.com>
2022-02-09 15:56:14 +0100

    added read more button

--- src/components/PostCard.vue
+++ src/components/PostCard.vue
@@ -14,16 +14,19 @@
                         <h2>{{posts[17].title}}</h2>
                         <p class="text">{{posts[17].content}}</p>
                         <span class="dots">[...]</span>
-                        <div>
-                            <span><i class="far fa-user"></i><a href="#">{{posts[17].author}}</a></span>
-                            <span>
-                                <i class="far fa-folder"></i>
-                                <a href="#" v-for="(genre,i) in posts[17].category" :key="i">
-                                <span v-if="i > 0">{{genre}}</span>
-                                <span v-else>{{genre + ', '}}</span>
-                                </a>
-                                </span>
-                            <span><i class="far fa-comments"></i><a href="#">12 Comments</a></span>
+                        <div class="post-data">
+                            <div>
+                                <span><i class="far fa-user"></i><a href="#">{{posts[17].author}}</a></span>
+                                <span>
+                                    <i class="far fa-folder"></i>
+                                    <a href="#" v-for="(genre,i) in posts[17].category" :key="i">
+                                    <span v-if="i > 0">{{genre}}</span>
+                                    <span v-else>{{genre + ', '}}</span>
+                                    </a>
+                                    </span>
+                                <span><i class="far fa-comments"></i><a href="#">12 Comments</a></span>
+                            </div>
+                            <button>READ MORE</button>
                         </div>
                     </div>
                 </div>
@@ -44,16 +47,19 @@
                         <h2>{{posts[6].title}}</h2>
                         <p class="text">{{posts[6].content}}</p>
                         <span class="dots">[...]</span>
-                        <div>
-                            <span><i class="far fa-user"></i><a href="#">{{posts[6].author}}</a></span>
-                            <span>
-                                <i class="far fa-folder"></i>
-                                <a href="#" v-for="(genre,i) in posts[6].category" :key="i">
-                                <span v-if="i > 0">{{genre}}</span>
-                                <span v-else>{{genre + ', '}}</span>
-                                </a>
-                                </span>
-                            <span><i class="far fa-comments"></i><a href="#">12 Comments</a></span>
+                        <div class="post-data">
+                            <div>
+                                <span><i class="far fa-user"></i><a href="#">{{posts[6].author}}</a></span>
+                                <span>
+                                    <i class="far fa-folder"></i>
+                                    <a href="#" v-for="(genre,i) in posts[6].category" :key="i">
+                                    <span v-if="i > 0">{{genre}}</span>
+                                    <span v-else>{{genre + ', '}}</span>
+                                    </a>
+                                    </span>
+                                <span><i class="far fa-comments"></i><a href="#">12 Comments</a></span>
+                            </div>
+                            <button>READ MORE</button>
                         </div>
                     </div>
                 </div>
@@ -74,16 +80,19 @@
                         <h2>{{posts[7].title}}</h2>
                         <p class="text">{{posts[7].content}}</p>
                         <span class="dots">[...]</span>
-                        <div>
-                            <span><i class="far fa-user"></i><a href="#">{{posts[7].author}}</a></span>
-                            <span>
-                                <i class="far fa-folder"></i>
-                                <a href="#" v-for="(genre,i) in posts[7].category" :key="i">
-                                <span v-if="i > 0">{{genre}}</span>
-                                <span v-else>{{genre + ', '}}</span>
-                                </a>
-                                </span>
-                            <span><i class="far fa-comments"></i><a href="#">12 Comments</a></span>
+                        <div class="post-data">
+                            <div>
+                                <span><i class="far fa-user"></i><a href="#">{{posts[7].author}}</a></span>
+                                <span>
+                                    <i class="far fa-folder"></i>
+                                    <a href="#" v-for="(genre,i) in posts[7].category" :key="i">
+                                    <span v-if="i > 0">{{genre}}</span>
+                                    <span v-else>{{genre + ', '}}</span>
+                                    </a>
+                                    </span>
+                                <span><i class="far fa-comments"></i><a href="#">12 Comments</a></span>
+                            </div>
+                            <button>READ MORE</button>
                         </div>
                     </div>
                 </div>
@@ -104,16 +113,19 @@
                         <h2>{{posts[17].title}}</h2>
                         <p class="text">{{posts[9].content}}</p>
                         <span class="dots">[...]</span>
-                        <div>
-                            <span><i class="far fa-user"></i><a href="#">{{posts[9].author}}</a></span>
-                            <span>
-                                <i class="far fa-folder"></i>
-                                <a href="#" v-for="(genre,i) in posts[9].category" :key="i">
-                                <span v-if="i > 0">{{genre}}</span>
-                                <span v-else>{{genre + ', '}}</span>
-                                </a>
-                                </span>
-                            <span><i class="far fa-comments"></i><a href="#">12 Comments</a></span>
+                        <div class="post-data">
+                            <div>
+                                <span><i class="far fa-user"></i><a href="#">{{posts[9].author}}</a></span>
+                                <span>
+                                    <i class="far fa-folder"></i>
+                                    <a href="#" v-for="(genre,i) in posts[9].category" :key="i">
+                                    <span v-if="i > 0">{{genre}}</span>
+                                    <span v-else>{{genre + ', '}}</span>
+                                    </a>
+                                    </span>
+                                <span><i class="far fa-comments"></i><a href="#">12 Comments</a></span>
+                            </div>
+                            <button>READ MORE</button>
                         </div>
                     </div>
                 </div>
@@ -136,16 +148,19 @@
                         <h2>{{posts[16].title}}</h2>
                         <p class="text">{{posts[16].content}}</p>
                         <span class="dots">[...]</span>
-                        <div>
-                            <span><i class="far fa-user"></i><a href="#">{{posts[16].author}}</a></span>
-                            <span>
-                                <i class="far fa-folder"></i>
-                                <a href="#" v-for="(genre,i) in posts[16].category" :key="i">
-                                <span v-if="i > 0">{{genre}}</span>
-                                <span v-else>{{genre + ', '}}</span>
-                                </a>
-                                </span>
-                            <span><i class="far fa-comments"></i><a href="#">12 Comments</a></span>
+                        <div class="post-data">
+                            <div>
+                                <span><i class="far fa-user"></i><a href="#">{{posts[16].author}}</a></span>
+                                <span>
+                                    <i class="far fa-folder"></i>
+                                    <a href="#" v-for="(genre,i) in posts[16].category" :key="i">
+                                    <span v-if="i > 0">{{genre}}</span>
+                                    <span v-else>{{genre + ', '}}</span>
+                                    </a>
+                                    </span>
+                                <span><i class="far fa-comments"></i><a href="#">12 Comments</a></span>
+                            </div>
+                            <button>READ MORE</button>
                         </div>
                     </div>
                 </div>
@@ -192,6 +207,10 @@ export default {
            width: calc(100% - 50px);
            padding: 0px 20px;
            line-height: 25px;
+           .post-data{
+               display: flex;
+               justify-content: space-between;
+           }
            h2{
                margin-bottom: 20px;
                color: #333;
@@ -212,6 +231,14 @@ export default {
             a{
                 color: $main-blue-color;
                 margin-right: 5px;
+            }
+            button{
+                background-color: #fff;
+                border: 2px solid #888;
+                padding: 7px;
+                border-radius: 8px;
+                color: #888;
+                cursor: pointer;
             }
        }
     }
